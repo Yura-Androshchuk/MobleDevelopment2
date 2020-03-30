faf035b84e300c39969d16031daac540055b0d2c
Yura-Androshchuk <yurathebestone@gmail.com>
2020-03-30 22:14:59 +0300

    copy-paste

--- MobDevelopmenLab2/app/src/main/java/com/example/mobdevelopmenlab2/ContentFragment.java
+++ MobDevelopmenLab2/app/src/main/java/com/example/mobdevelopmenlab2/ContentFragment.java
@@ -1,6 +1,7 @@
 package com.example.mobdevelopmenlab2;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -11,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.util.Date;
@@ -20,12 +22,44 @@ public class ContentFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_content,container,false );
     }
     @Override
     public void onStart()
     {
         super.onStart();
+        // получаем объект RadioGroup
+        RadioGroup radGrp =  (RadioGroup)getActivity().findViewById(R.id.radios);
+        // обработка переключения состояния переключателя
+        radGrp.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup arg0, int id) {
+                TextView textView = (TextView) getActivity().findViewById(R.id.textView);
+                EditText editText = (EditText) getActivity().findViewById(R.id.editText);
+                switch(id) {
+                    case R.id.serif:
+                        textView.setText("");
+                        textView.setTypeface(Typeface.create("serif", Typeface.NORMAL));
+                        break;
+                    case R.id.casual:
+                        textView.setText("");
+                        textView.setTypeface(Typeface.create("casual", Typeface.NORMAL));
+                        break;
+                    case R.id.cursive:
+                        textView.setText("");
+                        textView.setTypeface(Typeface.create("cursive", Typeface.NORMAL));
+                        break;
+                    case R.id.monospace:
+                        textView.setText("");
+                        textView.setTypeface(Typeface.create("monospace", Typeface.NORMAL));
+                        break;
+                    default:
+                        // textView.setTypeface(Typeface.create("serif", Typeface.NORMAL));
+                        break;
+                }
+            }});
         Button button = (Button) getActivity().findViewById(R.id.OK);
         button.setOnClickListener(new View.OnClickListener(){
          @Override
--- MobDevelopmenLab2/app/src/main/java/com/example/mobdevelopmenlab2/MainActivity.java
+++ MobDevelopmenLab2/app/src/main/java/com/example/mobdevelopmenlab2/MainActivity.java
@@ -16,37 +16,7 @@ public class MainActivity extends FragmentActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // получаем объект RadioGroup
-        RadioGroup radGrp =  (RadioGroup)findViewById(R.id.radios);
-        // обработка переключения состояния переключателя
-        radGrp.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
 
-            @Override
-            public void onCheckedChanged(RadioGroup arg0, int id) {
-                TextView textView = (TextView) findViewById(R.id.textView);
-                EditText editText = (EditText) findViewById(R.id.editText);
-                switch(id) {
-                    case R.id.serif:
-                        textView.setText("");
-                        textView.setTypeface(Typeface.create("serif", Typeface.NORMAL));
-                        break;
-                    case R.id.casual:
-                        textView.setText("");
-                        textView.setTypeface(Typeface.create("casual", Typeface.NORMAL));
-                        break;
-                    case R.id.cursive:
-                        textView.setText("");
-                        textView.setTypeface(Typeface.create("cursive", Typeface.NORMAL));
-                        break;
-                    case R.id.monospace:
-                        textView.setText("");
-                        textView.setTypeface(Typeface.create("monospace", Typeface.NORMAL));
-                        break;
-                    default:
-                        // textView.setTypeface(Typeface.create("serif", Typeface.NORMAL));
-                        break;
-                }
-            }});
 
     }
 
